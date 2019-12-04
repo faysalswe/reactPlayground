caeaaf0fe8509137b17fb88804d4e87c87811105
Faysal Ahmmad <faysalswe@gmail.com>
2019-12-05 00:36:31 +0600

    added my pic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-
+import faysal from './assets/faysal.png';
 function App() {
   return (
-    <div className="App">
-      <h1>React Component</h1>
+    <div className="container">
+      <img className="img-container" src={faysal} alt="faysal"/>
+      <h1>Faysal Ahmmad</h1>
+      <h3>I am a software engineer, professionally working on asp net core and angular stack.</h3>
+       <h3> Also love deployment, automation.</h3>
     </div>
   );
 }
