ab1be8f2112fb062688c3badeefdd0afdb47b7a8
Faysal Ahmmad <faysalswe@gmail.com>
2019-12-08 00:00:53 +0600

    update style

--- src/components/todo/todo-form.js
+++ src/components/todo/todo-form.js
@@ -16,25 +16,22 @@ export default class TodoForm extends Component {
     }
     render() {
         return (
-        <div className="form-inline">
-            <div className="input-group mb-2 mr-sm-2">
-                <div className="input-group-prepend">
-                    <div className="input-group-text"> --> </div>
-                </div>
-                <input type="text" 
-                className="form-control" 
-                name="title"
-                placeholder="Your task"
-                value={this.state.title}
-                onChange={this.handleChange}/>
-            </div>
+            <div className="form-inline">
+                <div className="input-group mb-2">
+                    <input type="text"
+                        className="form-control"
+                        name="title"
+                        placeholder="Your task"
+                        value={this.state.title}
+                        onChange={this.handleChange} />
 
-            <div className="btn-group">
-                <button className="btn btn-primary" onClick={this.addTodo} > New </button>
-                <button className="btn btn-secondary" 
-                onClick={this.props.filterTodo.bind(this, this.state.title)}> Search </button>
+                    <div className="btn-group">
+                        <button className="btn btn-primary" onClick={this.addTodo} > New </button>
+                        <button className="btn btn-secondary"
+                            onClick={this.props.filterTodo.bind(this, this.state.title)}> Search </button>
+                    </div>
+                </div>
             </div>
-        </div>
         )
     }
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                <h4> This router todo applicatoin. Inspired by traversy carsh course. </h4>
+                <h4> This router todo application. Inspired by traversy crash course. </h4>
             </div>
         )
     }
