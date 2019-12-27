6bb526e24ba185b3452533a5447880b7395a0e15
Faysal Ahmmad <faysalswe@gmail.com>
2019-12-28 01:02:11 +0600

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import {CardList} from './components/card-list/card-list.component'
+import { SearchBox } from "./components/search-box/search-box.component";
 import './App.css';
-import faysal from './assets/faysal.png';
-function App() {
-  return (
-    <div className="container">
-      <img className="img-container" src={faysal} alt="faysal"/>
-      <h1>Faysal Ahmmad</h1>
-      <h3>I am a software engineer, professionally working on asp net core and angular stack.</h3>
-       <h3> Also love deployment, automation.</h3>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      search: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(monsters => this.setState({ monsters: monsters }))
+  }
+
+  render() {
+    const { monsters, search } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(search.toLowerCase())  
+    );
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="search monster"
+          handleChange={e => this.setState({search: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
